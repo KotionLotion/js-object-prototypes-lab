1161c38449889eb7d77e1d44b0346e49480196a8
Jesse Melgar <2020151717@ub.edu.bz>
2026-01-30 23:37:19 -0600

    Complete Part 1: Function declarations and expressions

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+function calculateArea(length, width) {
+    return `Area: ${length * width}`;
+};
+console.log(calculateArea(5, 10));
+console.log(calculateArea(7, 3));
+console.log(calculateArea(4, 4));
---
+++ exercise2.js
@@ -0,0 +1,7 @@
+const calculatePerimeter = (length, width) => {
+    return `Perimeter: ${2 * (length + width)}`;
+};
+
+console.log(calculatePerimeter(5, 10));
+console.log(calculatePerimeter(7, 3));
+console.log(calculatePerimeter(4, 4));
